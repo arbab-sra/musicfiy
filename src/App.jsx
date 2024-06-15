58134911d7eb10d315da7c4afa4bc6de5b8a9f8e
arbab-killer <mr.crazymemes76@gmail.com>
2024-06-15 12:07:53 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ const App = () => {
       </div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={"kalfj"} />
           <Route path="/discover" element={<Discover />} />
           <Route path="/albums" element={<Album />} />
           <Route path="/artists" element={<Artists />} />
