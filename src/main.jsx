1d20d55d12432a505039f55e7313c590d63d665a
arbab-killer <mr.crazymemes76@gmail.com>
2024-07-28 20:41:14 +0530

    Analytics add

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Song} from "./constext/useContext.jsx";
-
+import { Song } from "./constext/useContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Song>
       <App />
+      <Analytics />
     </Song>
   </React.StrictMode>
 );
